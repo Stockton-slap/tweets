0addeb6b8b122966d4e99fab1ba4249f79ae49b7
Andrey Yurchenko <spree92spree92@gmail.com>
2023-04-27 17:11:09 +0300

    add images and pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "modern-normalize";
+import { Route, Routes } from "react-router-dom";
+
+import { Home, Tweet } from "./pages";
 
 function App() {
-  return <div>1231231</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/tweets" element={<Tweet />}></Route>
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+import Home from "./Home";
+import Tweet from "./Tweet";
+
+export { Home, Tweet };
