f15628b2092975434ac23db89e732572aaf891f5
Andrey Yurchenko <spree92spree92@gmail.com>
2023-05-01 11:07:21 +0300

    arrange code

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -3,7 +3,6 @@ import PropTypes from "prop-types";
 import { Circle, CircleWrapper } from "./Avatar.styled";
 
 const Avatar = ({ avatar }) => {
-  console.log(typeof avatar);
   return (
     <CircleWrapper>
       <Circle avatar={avatar} />
